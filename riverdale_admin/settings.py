55646c32f9092393c3e593c419e0a17f9e374928
Gurinder01 <gurinderpalsingh612@gmail.com>
2024-03-14 18:26:02 +1100

    Add static files

--- riverdale_admin/settings.py
+++ riverdale_admin/settings.py
@@ -118,10 +118,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+# Example settings
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR/'assets'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
